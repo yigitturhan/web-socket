c97a36e01794ec3759ce6ed87e22d4c8b583e384
Ahmet Yiğit Turhan <112410497+yigitturhan@users.noreply.github.com>
2023-12-27 22:28:03 +0300

    Update tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -4,18 +4,39 @@ import socket
 
 host = ""  # empty
 port = 12345  # The port specified in server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((host, port))
-    s.listen()
-    print(f"Server listening on {host}:{port}")
-    conn, addr = s.accept()
-    print(f"Connection established from {addr}")
-    with open("received_model.obj", "wb") as f:
-        while True:
-            chunk = conn.recv(1024)
-            if not chunk:
-                break
-            f.write(chunk)
-
+model_count = 0
+def get_large_objects(host, port):
+    global model_count
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f"Server listening on {host}:{port}")
+        conn, addr = s.accept()
+        print(f"Connection established from {addr}")
+        with open("received_model_"+str(model_count)+".obj", "wb") as f:
+            while True:
+                chunk = conn.recv(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+        model_count += 1
     print("File received successfully.")
     conn.close()
+
+def get_small_objects(host, port):
+    global model_count
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f"Server listening on {host}:{port}")
+        conn, addr = s.accept()
+        print(f"Connection established from {addr}")
+        with open("received_model_"+str(model_count)+".obj", "wb") as f:
+            chunk = conn.recv(1024)
+            f.write(chunk)
+        model_count += 1
+    print("File received successfully.")
+    conn.close()
+
+get_large_objects(host, port)
+get_small_objects(host, port)
