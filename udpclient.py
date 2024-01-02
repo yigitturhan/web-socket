0ad99ab9357c666e73a7f08d5c00b95bcb634956
Ahmet Yiğit Turhan <turhan.yigit@metu.edu.tr>
2024-01-02 19:36:10 +0300

    update

--- udpclient.py
+++ udpclient.py
@@ -1,37 +1,42 @@
 import socket
+import hashlib
 dest = ("", 12345)
 type = 'utf-8'
 def get_objects(dest):
-    g = 0
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind(dest)
         while True:
-            
             header_received, state = False, 0
             while not header_received: #header ı alma loopu
                 message, addr = s.recvfrom(1024)
+                hash, header = message[:64], message[64:]
                 message = message.decode(type) #header endse tüm filelar gelmiş demek
                 if message == "END":
                     s.sendto("OK".encode(type), addr) #işlem bitmişse ok gönder
                     return
                 try:
+                    if compute_sha256(header) != hash:
+                        s.sendto("NACKHEADER".encode(type), addr) 
+                        continue
                     fileName, fileSize = message.split('_') #filename ve size çek
-                    fileSize, receivedBytes = int(fileSize), 0
+                    fileName, fileSize, receivedBytes =  fileName[64:], int(fileSize), 0
                     s.sendto("ACK_HEADER".encode(type), addr) #filename ve size ı çekebiliyosa ack header gönder
                     header_received = True #looptan çık
                 except:
                     pass
             with open(fileName, "wb") as f: #dosyaya yazma
                 while receivedBytes < fileSize: #tüm paketleri bekle
-                    message = s.recv(1025) #mesaj al
-                    if message[0] != state + 48:
+                    message = s.recv(1089) #mesaj al
+                    hash, data = message[1:65], message[65:]
+                    if message[0] != state + 48 or hash != compute_sha256(str(message[0]-48).encode(type) + data):
                         s.sendto(("ACK_DATA_" + str(state)).encode(type), addr)
                         continue
                     if not message:
                         break
-                    f.write(message[1:])
+                    f.write(data)
                     s.sendto(("ACK_DATA_" + str(state)).encode(type), addr) #ack 0 ya da 1 gönder (state e göre)
                     receivedBytes += 1024 #güncelle ki looptan çıksın
                     state = 1 - state #state değiştirme
-
+def compute_sha256(data):
+    return hashlib.sha256(data).hexdigest().encode(type)
 get_objects(dest)
--- udpserver.py
+++ udpserver.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import hashlib
 type = 'utf-8'
 host, port = "172.17.0.2" , 12345 # clients ip address and the port
 def send_object(pathlist, host, port):
@@ -13,21 +14,24 @@ def send_object(pathlist, host, port):
             data = read_data(pathlist[ind])
             header = create_header(pathlist[ind])
             packet_count = len(data)
+            hash_of_header = compute_sha256(header)
             while not header_sent: #header gönderilene kadar deniyo
-                s.sendto(header, dest)
+                s.sendto(hash_of_header+header, dest)
+                print(hash_of_header)
                 try:
                     message = s.recv(10).decode(type)
+                    print(message)
                     if message == "ACK_HEADER": #ack header gelirse looptan çıkıyo
                         header_sent = True
                 except:
                     pass
             while True: #tüm paketleri göndermek için loop
-               
                 while not current_packet_sent:  #şuanki paketi onaylamak için loop
-                    s.sendto(str(state).encode(type) + data[current_packet], dest) #paketi gönder
+                    print("vvvv")
+                    hash = compute_sha256(str(state).encode(type) + data[current_packet])
+                    s.sendto(str(state).encode(type) + hash + data[current_packet], dest) #paketi gönder
                     try:
                         message = s.recv(10).decode(type)
-                        print(message)
                         if (message == "ACK_DATA_1" and state == 1) or (message == "ACK_DATA_0" and state == 0): #state ve ack uyuyor mu
                             current_packet_sent = True
                             current_packet += 1 #bi sonraki pakete geç
@@ -47,9 +51,6 @@ def send_object(pathlist, host, port):
                 pass
 
 
-
-
-
 def create_header(path):
     filename = path[14:]
     bytecount = os.path.getsize(path)
@@ -66,7 +67,8 @@ def read_data(path):
             data.append(chunk)
     return data
 
-
+def compute_sha256(data):
+    return hashlib.sha256(data).hexdigest().encode(type)
 paths = ["/root/objects/large-0.obj","/root/objects/large-1.obj","/root/objects/large-2.obj",
 "/root/objects/small-0.obj","/root/objects/small-1.obj","/root/objects/small-2.obj"]
 send_object(paths, host, port)
