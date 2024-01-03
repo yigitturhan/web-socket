3fc09b54fe584207eba2dc7a47e77cd1c907f531
Ahmet Yiğit Turhan <turhan.yigit@metu.edu.tr>
2024-01-03 19:32:54 +0300

    update

--- tcpclient.py
+++ tcpclient.py
@@ -31,7 +31,95 @@ def get_objects(host, port):
             print(message)
         print("file received")
         conn.send(b"RECEIVED")
-         message = conn.recv(20)
+        message = conn.recv(20)
+        message = message.decode('utf-8')
+        print(message)
+        fileName, fileSize = message.split("_")
+        fileSize, received_bytes = int(fileSize), 0
+        conn.send(b"NO_PROBLEM")
+        flag, last_byte = True, 0
+        with open(fileName, "wb") as f:
+            while received_bytes < fileSize:
+                chunk = conn.recv(1024)
+                last_byte = chunk[-5:]
+                if last_byte == b"bitti":
+                    flag = False
+                    break
+                received_bytes += len(chunk)
+                f.write(chunk)
+        print("çiktim")
+        if flag and last_byte != b"bitti":
+            message = conn.recv(5)
+            print(message)
+        print("file received")
+        conn.send(b"RECEIVED")
+        message = conn.recv(20)
+        message = message.decode('utf-8')
+        print(message)
+        fileName, fileSize = message.split("_")
+        fileSize, received_bytes = int(fileSize), 0
+        conn.send(b"NO_PROBLEM")
+        flag, last_byte = True, 0
+        with open(fileName, "wb") as f:
+            while received_bytes < fileSize:
+                chunk = conn.recv(1024)
+                last_byte = chunk[-5:]
+                if last_byte == b"bitti":
+                    flag = False
+                    break
+                received_bytes += len(chunk)
+                f.write(chunk)
+        print("çiktim")
+        if flag and last_byte != b"bitti":
+            message = conn.recv(5)
+            print(message)
+        print("file received")
+        conn.send(b"RECEIVED")
+        message = conn.recv(20)
+        message = message.decode('utf-8')
+        print(message)
+        fileName, fileSize = message.split("_")
+        fileSize, received_bytes = int(fileSize), 0
+        conn.send(b"NO_PROBLEM")
+        flag, last_byte = True, 0
+        with open(fileName, "wb") as f:
+            while received_bytes < fileSize:
+                chunk = conn.recv(1024)
+                last_byte = chunk[-5:]
+                if last_byte == b"bitti":
+                    flag = False
+                    break
+                received_bytes += len(chunk)
+                f.write(chunk)
+        print("çiktim")
+        if flag and last_byte != b"bitti":
+            message = conn.recv(5)
+            print(message)
+        print("file received")
+        conn.send(b"RECEIVED")
+        message = conn.recv(20)
+        message = message.decode('utf-8')
+        print(message)
+        fileName, fileSize = message.split("_")
+        fileSize, received_bytes = int(fileSize), 0
+        conn.send(b"NO_PROBLEM")
+        flag, last_byte = True, 0
+        with open(fileName, "wb") as f:
+            while received_bytes < fileSize:
+                chunk = conn.recv(1024)
+                last_byte = chunk[-5:]
+                if last_byte == b"bitti":
+                    flag = False
+                    break
+                received_bytes += len(chunk)
+                f.write(chunk)
+        print("çiktim")
+        if flag and last_byte != b"bitti":
+            message = conn.recv(5)
+            print(message)
+        print("file received")
+        conn.send(b"RECEIVED")
+        message = conn.recv(20)
         message = message.decode('utf-8')
         print(message)
         fileName, fileSize = message.split("_")
