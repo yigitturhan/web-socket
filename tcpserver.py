c709acad1b0265c26f4d4e3c5d936e1804e0843d
Furkan Numanoğlu <107202278+furkannumanoglu@users.noreply.github.com>
2024-01-04 03:52:43 +0300

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -1,4 +1,3 @@
-import os
 import socket
 import time
 type = "utf-8"
@@ -29,8 +28,7 @@ def send_file(s, filePath):
                     chunk = f.read(1024)
                 if not chunk:
                     break
-                s.sendall(chunk)
-                flag = False
+                s.sendall(chunk)                flag = False
             except Exception as e:
                 print(e)
                 flag = True
@@ -39,8 +37,9 @@ def send_file(s, filePath):
 host = "172.17.0.2"
 port = 12345
 dest = (host, port)
-send_object(["/root/objects/large-0.obj", "/root/objects/small-0.obj", "/root/objects/large-1.obj", "/root/objects/small-1.obj", 
+send_object(["/root/objects/large-0.obj", "/root/objects/small-0.obj", "/root/objects/large-1.obj", "/root/objects/small-1.obj",
 "/root/objects/large-2.obj", "/root/objects/small-2.obj","/root/objects/large-3.obj", "/root/objects/small-3.obj",
 "/root/objects/large-4.obj", "/root/objects/small-4.obj","/root/objects/large-5.obj", "/root/objects/small-5.obj",
 "/root/objects/large-6.obj", "/root/objects/small-6.obj","/root/objects/large-7.obj", "/root/objects/small-7.obj",
 "/root/objects/large-8.obj", "/root/objects/small-8.obj","/root/objects/large-9.obj", "/root/objects/small-9.obj"], dest)
+
