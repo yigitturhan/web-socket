2adf5c71f14904703fecb0784d91dbdc21cf3bd5
Furkan Numanoğlu <107202278+furkannumanoglu@users.noreply.github.com>
2024-01-05 06:32:38 +0300

    final update

--- udpserver.py
+++ udpserver.py
@@ -16,9 +16,18 @@ def send_object(pathlist, host, port):
     encoded_ok_hash = compute_sha256(encoded_ok)
     dest, end_sent = (host, port), False
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.settimeout(0.5) #1 saniye geçerse excepte girmesini sa�~_lıyo
+        rtt_sum, rtt_count = 0, 0
+        while rtt_count < 10:
+            rtt = calculate_rtt(s,dest,encoded_ok,encoded_pipe)
+            print(rtt)
+            if rtt != 0:
+                rtt_sum += rtt
+                rtt_count +=1
+        rtt = rtt_sum / rtt_count
+        if rtt < 0.01:
+            rtt = 0.01
+        s.settimeout(rtt) #1 saniye geçerse excepte girmesini sa�~_lıyo
         s.connect(dest)
-        print("Connection established with "+host+" at the port number "+str(port))
         data_list = read_data(pathlist)
         index_list = get_index_list(data_list)
         for ind in range(len(pathlist)):
@@ -26,18 +35,16 @@ def send_object(pathlist, host, port):
             hash_of_header = compute_sha256(header)
             while True: #header gönderilene kadar deniyo
                 s.sendto(hash_of_header+header, dest)
-                print("gönderdim"))
                 try:
                     message = s.recv(128) #hashli gelsin
-                    print(message)
                     hash_sha = message[:64]
                     if compute_sha256(message[64:]) == hash_sha and message[64:] == encoded_ack_header:
                         break
                 except Exception as e:
                     print(e)
-        print("ciktim")
-
-        while True:
+        counter = 0
+        while counter < 20:
+            counter += 1
             s.sendto(encoded_end_header_hash + encoded_end_header, dest)
             try:
                 message = s.recv(128) #hashli gelsin
@@ -53,10 +60,9 @@ def send_object(pathlist, host, port):
                     packets_to_send = data_list[x][y]
                     indexes_of_packets = str(y).encode(type)
                     file_name_2 = pathlist[x][14:]
-                    file_names_as_bytes = file_name_2.encode(type)                    
+                    file_names_as_bytes = file_name_2.encode(type)
                     hashes = compute_sha256((file_name_2 + "|" + str(y)).encode(type) + encoded_pipe + data_list[x][y])
                     hashed_packets = hashes+file_names_as_bytes+encoded_pipe+indexes_of_packets+encoded_pipe+packets_to_send
-                    print(indexes_of_packets)
                     s.sendto(hashed_packets, dest)
                 for i in range(len(index_list)):
                     message = s.recv(1024)
@@ -65,27 +71,26 @@ def send_object(pathlist, host, port):
                     rec_index = int(rec_index)
                     if ok == "OK" and hash == compute_sha256(("OK_"+ rec_filename+"_"+str(rec_index)).encode(type)):
                         try:
-                            index_list.remove((get_index_of_file(pathlist,rec_filename),rec_index))                        except:
+                            index_list.remove((get_index_of_file(pathlist,rec_filename),rec_index))
+                        except:
                             pass
 
             except Exception as e:
                 print(e)
-        while not end_sent: #tüm dosyalar gitti mi kontrolü
+        counter = 0
+        while not end_sent and counter < 15: #tüm dosyalar gitti mi kontrol
+            counter += 1
             s.sendto(encoded_end_hash + encoded_end, dest)
-            print("bura1")
             try:
                 message = s.recv(1024) #kar�~_ıya end gönder ok b
-                print(message)
                 hash_mes = message[:64]
                 if hash_mes == encoded_ok_hash:
                     end_sent = True
                     s.sendto(encoded_end_hash + encoded_end,dest)
-                    print("All files are sent and the connection is closed.")
             except Exception as e:
                 print(e)
     end = time.time()
     return end - start
-
 
 def create_header(path):
     filename = path[14:]
@@ -110,7 +115,6 @@ def read_data(pathlist):
                 temp.append(chunk)
             data.append(temp)
     return data
-
 def compute_sha256(data):
     return hashlib.sha256(data).hexdigest().encode(type)
 
@@ -118,8 +122,20 @@ def compute_sha256(data):
 def get_index_list(data_list):
     res = [(i,j) for i in range(len(data_list)) for j in range(len(data_list[i]))]
     return res
-
-paths = ["/root/objects/large-0.obj","/root/objects/large-1.obj","/root/objects/large-2.obj",
+def calculate_rtt(s, dest, encoded_ok, encoded_pipe):
+    counter = 0
+    s.settimeout(0.3)
+    a, b = 0, 0
+    s.sendto(encoded_pipe,dest)
+    try:
+        a = time.time()
+        s.recv(10)
+        b = time.time()
+        return b-a
+    except:
+        pass
+    return 0
+paths= ["/root/objects/large-0.obj","/root/objects/large-1.obj","/root/objects/large-2.obj",
 "/root/objects/large-3.obj","/root/objects/large-4.obj","/root/objects/large-5.obj",
 "/root/objects/large-6.obj","/root/objects/large-7.obj","/root/objects/large-8.obj",
 "/root/objects/large-9.obj","/root/objects/small-0.obj","/root/objects/small-1.obj",
@@ -128,8 +144,4 @@ paths = ["/root/objects/large-0.obj","/root/objects/large-1.obj","/root/objects/
 "/root/objects/small-8.obj","/root/objects/small-9.obj"]
 print(send_object(paths, host, port))
 
-
-
-
-
-
+        
